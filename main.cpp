f586e24e741930717cfd530c5196f6868c3df786
Žygimantas <zyg.sid2@gmail.com>
2019-06-10 15:16:50 +0300

    Added many functions and operators

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     int kartai = 0;
 
     timer.startClock();
-    int sz = 100000000;  // 100000, 1000000, 10000000, 100000000
+    int sz = 10000000;  // 100000, 1000000, 10000000, 100000000
     vek::vector<int> v1;
     for (int i = 1; i <= sz; i++) {
         if(v1.capacity() == v1.size())
@@ -20,22 +20,43 @@ int main() {
 
     timer.startClock();
     std::vector<int> v2;
-        for (int j = 1; j <= sz; j++)
+        for (int j = sz+1; j <= sz+sz; j++)
             v2.push_back(j);
     timer.endClock("Originalaus vektorio uzpildymas");
     cout << sz << " elementu uzpildymas" << endl;
     cout << "Savadarbiame vektoriuje ivyko " << kartai << " kartus atminties perskirstymas" << endl;
 
+    vek::vector<int> v3;
+
+
+    //v1.resize(200);
+
 //    //Size Funkcija
-//    cout << "SIZE:" << endl;
-//    cout << v1.size() << endl;
-//    cout << v2.size() << endl;
-//
-//    //Capacity funkcija
-//    cout << "CAP:" << endl;
-//    cout << v1.capacity() << endl;
-//    cout << v2.capacity() << endl;
-//
+    cout << "SIZE:" << endl;
+    cout << v1.size() << endl;
+    cout << v2.size() << endl;
+    cout << v3.size() << endl;
+
+    //Capacity funkcija
+    cout << "CAP:" << endl;
+    cout << v1.capacity() << endl;
+    cout << v2.capacity() << endl;
+    cout << v3.capacity() << endl;
+
+    v3.resize(100);
+    v1.swap(v3);
+
+    cout << "SIZE:" << endl;
+    cout << v1.size() << endl;
+    cout << v2.size() << endl;
+    cout << v3.size() << endl;
+
+    //Capacity funkcija
+    cout << "CAP:" << endl;
+    cout << v1.capacity() << endl;
+    cout << v2.capacity() << endl;
+    cout << v3.capacity() << endl;
+
 //    //Push_back funkcija
 //    v1.push_back(14);
 //    v2.push_back(14);
--- vector.h
+++ vector.h
@@ -8,7 +8,28 @@
 #include "main.h"
 
 namespace vek {
-    template <typename T>
+    template < typename T >
+    class vector;
+
+    template < typename T >
+    bool operator== (const vector <T>&, const vector <T>&);
+
+    template < typename T >
+    bool operator!= (const vector <T>&, const vector <T>&);
+
+    template < typename T >
+    bool operator< (const vector <T>&, const vector <T>&);
+
+    template < typename T >
+    bool operator> (const vector <T>&, const vector <T>&);
+
+    template < typename T >
+    bool operator<= (const vector <T>&, const vector <T>&);
+
+    template < typename T >
+    bool operator>= (const vector <T>&, const vector <T>&);
+
+    template < typename T >
     class vector {
     private:
         size_t size_;
@@ -18,8 +39,41 @@ namespace vek {
         //Konstruktoriai / destruktoriai---------------------------------------
         vector() : size_{ 0 }, elem_{ new T[size_] }, cap_{ 0 } {};
         vector(size_t size) : size_{ size }, elem_{ new T[size] }, cap_{ size } {};
-        vector(size_t size, T value) : size_{ size }, elem_{ new T[size] }, cap_{ size } { fill_n(elem_, size, value); }
+        vector(size_t size, T value) : size_{ size }, elem_{ new T[size] }, cap_{ size } { std::fill_n(elem_, size, value); }
+        vector(vector&& a)
+                : size_{ a.size_ },
+                  elem_{ a.elem_ }, cap_{ a.cap_ } {
+            a.elem_ = nullptr;
+            a.size_ = 0;
+            a.cap_ = 0;
+        }
+        vector(std::initializer_list<T> il)
+                : size_{ static_cast<std::size_t>(il.size()) },
+                  cap_{ size_ },
+                  elem_{ new T[il.size()] } {
+            std::copy(il.begin(), il.end(), elem_);
+        }
         ~vector() { delete[] elem_; }
+
+        vector& operator= (const vector& v){
+            if(&v == this) return *this;
+            T *p = new T[v.size_];
+            for(int i = 0; i != v.size_; i++) p[i] = v.elem_[i];
+            delete[] elem_;
+            elem_ = p;
+            size_ = v.size_;
+            return *this;
+        }
+
+        vector& operator= (const vector&& v){
+            if(&v == this) return *this;
+            delete[] elem_;
+            elem_ = v.elem_;
+            size_ = v.size_;
+            v.elem_ = nullptr;
+            v.size_ = 0;
+            return *this;
+        }
 
         //Narių tipai----------------------------------------------------------
         typedef T *iterator;
@@ -46,12 +100,12 @@ namespace vek {
 
         //Iteratoriai----------------------------------------------------------
         iterator begin() {
-            if (size_ == 0) throw std::out_of_range("Empty vector!");
-            return elem_;
+            if (size_ > 0)
+                return elem_;
         }
         iterator end() {
-            if (size_ == 0) throw std::out_of_range("Empty vector!");
-            return elem_ + size_;
+            if (size_ > 0)
+                return elem_ + size_;
         }
 
         //Funkcijos------------------------------------------------------------
@@ -102,19 +156,129 @@ namespace vek {
             size_++;
         }
 
-        void resize(size_t n) {
+        iterator resize(size_t n, iterator it = 0) {
+            if (n == 0) {
+                size_ = 0;
+                cap_ = 2;
+                T *resize = new T[2];
+                delete[] elem_;
+                elem_ = resize;
+                return 0;
+            }
             size_t end;
-            if (n == cap_) return;
+            if (n == cap_) return it;
             T *resize = new T[n];
             if (n < size_) {
                 size_ = n;
                 end = n;
             }
             else end = size_;
+            auto pos = it - begin();
             std::move(&elem_[0], &elem_[end], resize);
             cap_ = n;
             delete[] elem_;
             elem_ = resize;
+            return begin() + pos;
+        }
+
+        void shrink_to_fit() {
+            if (size_ == cap_) return;
+            T *resize = new T[size_];
+            std::move(&elem_[0], &elem_[size_], resize);
+            cap_ = size_;
+            delete[] elem_;
+            elem_ = resize;
+        }
+
+        void swap(vector<T> & vec) {
+            size_t sw_sz = vec.size_;
+            size_t sw_cap = vec.cap_;
+            vector<T> sw_c(vec.size());
+            vector<T> sw_o(size_);
+            std::move(vec.begin(), vec.end(), sw_c.elem_);
+            std::move(&elem_[0], &elem_[size_], sw_o.elem_);
+            vec.size_ = size_;
+            vec.cap_ = cap_;
+            delete[] vec.elem_;
+            delete[] elem_;
+            size_ = sw_sz;
+            cap_ = sw_cap;
+            elem_ = sw_c.elem_;
+            vec.elem_ = sw_o.elem_;
+        }
+
+        T& at(size_t pos) {
+            if ((pos < 0) || (pos >= size_)) {
+                throw std::out_of_range("Out of array!");
+            }
+            return elem_[pos];
+        }
+
+        const_reference at(size_t pos)const {
+            if ((pos < 0) || (pos >= size_)) {
+                throw std::out_of_range("Out of array!");
+            }
+            return elem_[pos];
+        }
+
+        void assign(size_t count, const T& val) {
+            assign(0, count, val);
+        }
+
+        void assign(size_t first, size_t last, const T& val) {
+            size_ = last;
+            for (size_t i = first; (i < last) && (i < size_); i++)
+                elem_[i] = val;
+        }
+
+        T* data() noexcept{
+            return elem_;
+        }
+
+        void pop_back() {
+            if (!size_)
+                return;
+            else
+                size_--;
+        }
+
+        iterator insert(iterator pos, const T & a) {
+            insert(pos, 1, a);
+            return pos;
+        }
+
+        iterator insert(iterator pos, size_t b, const T & a) {
+            if (pos > elem_ + size_) throw std::out_of_range("Out of array!");
+            for(int i = 0; i < b; i++){
+                if (size_ >= cap_) {
+                    pos = resize(size_ * 2,pos);
+                }
+                size_++;
+                std::move(pos, pos + size_, pos + 1);
+                *pos = a;
+            }
+            return pos;
+        }
+
+        iterator insert(iterator pos, std::initializer_list<T> il){
+            if(size_+il.size() >= cap_){
+                pos = resize(size_ + il.size() * 2,pos);
+            }
+            std::move(pos, pos + size_, pos + il.size());
+            size_ += il.size();
+            std::move(il.begin(), il.end(), pos);
+            return pos;
+        }
+
+        iterator emplace(iterator it, const T & args) {
+            return insert(it, std::move(args));
+        }
+
+        void emplace_back(const T &args) {
+            return push_back(std::move(args));
+        }
+        void emplace_front(const T &args) {
+            return push_front(std::move(args));
         }
 
         //Operatoriai---------------------------------------------------------
@@ -123,8 +287,90 @@ namespace vek {
             return elem_[i];
         }
 
+        friend bool operator== <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator!= <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator< <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator> <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator<= <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator>= <T> (const vector <T>& left, const vector <T>& right);
 
     };
+
+    template < typename T >
+    bool operator== (const vector <T>& left, const vector <T>& right) {
+        if (left.size_ != right.size_)return false;
+        else
+            for (int i = 0; i < left.size_; ++i)
+                if (left[i] != right[i])return false;
+        return true;
+    }
+
+    template < typename T >
+    bool operator!= (const vector <T>& left, const vector <T>& right) {
+        return !(left == right);
+    }
+
+    template < typename T >
+    bool operator< (const vector <T>& left, const vector <T>& right) {
+
+        if(left.empty() && right.empty()) return false;
+        else if (left.empty()) return true;
+        else if (right.empty()) return false;
+        else {
+            int i = 0;
+            while(left[i]==right[i]){
+                if((i+1 > left.size_)||(i+1 > right.size_)) break;
+                i++;
+            }
+            if(left[i] < right[i]) return true;
+            else if(left[i] == right[i]){
+                if(left.size_ < right.size_) return true;
+                else return false;
+            }
+            else return false;
+        }
+    }
+
+    template < typename T >
+    bool operator> (const vector <T>& left, const vector <T>& right) {
+        return !(left < right);
+    }
+
+    template < typename T >
+    bool operator<= (const vector <T>& left, const vector <T>& right) {
+
+        if(left.empty() && right.empty()) return true;
+        else if (left.empty()) return true;
+        else if (right.empty()) return false;
+        else {
+            int i = 0;
+            while(left[i]==right[i]){
+                if((i+1 > left.size_)||(i+1 > right.size_)) break;
+                i++;
+            }
+            if(left[i] < right[i]) return true;
+            else if(left[i] == right[i]){
+                if(left.size_ < right.size_) return true;
+                else return true;
+            }
+            else return false;
+        }
+    }
+
+    template < typename T >
+    bool operator>= (const vector <T>& left, const vector <T>& right) {
+        return !(left <= right);
+    }
+    template  < typename T >
+    vector <T> operator+(const vector <T>& a, const vector <T>& b) {
+        if (a.size() != b.size())
+            throw std::runtime_error("Vector sizes not equal!");
+        auto size = a.size();
+        vector<T> c(size);
+        for (auto i = 0; i != a.size(); ++i) c[i] = a[i] + b[i];
+        return c;
+    }
+
 }
 
 #endif //VECTOR_VECTOR_H
